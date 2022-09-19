7a572ee89d5710fbcdc77995ab473a086b960abd
GudingStar <ashaisuzuki2022@gmail.com>
2022-09-19 11:22:32 -0700

    updateCommit

--- src/googleAuthenticator.js
+++ src/googleAuthenticator.js
@@ -1,137 +1,126 @@
 import React, { useEffect, useState } from "react";
-import speakeasy from 'speakeasy';
-import QRCode from 'qrcode';
+import speakeasy from "speakeasy";
+import QRCode from "qrcode";
 import { ethers } from "ethers";
 import { useWallet } from "use-wallet";
 import CryptoJS from "crypto-js";
+import axios from "axios";
 
 const GoogleAuthenticator = () => {
-    const [image,setImage] = useState();
-    const [secret,setSecret] = useState();
-    const [validCode,setValidCode] = useState();
-    const [isCodeValid, setIsCodeValid] = useState(null);
-    const [inputValue,setInputValue] = useState();
+  const [image, setImage] = useState();
+  const [secret, setSecret] = useState();
+  const [validCode, setValidCode] = useState();
+  const [isCodeValid, setIsCodeValid] = useState(0);
+  const [inputValue, setInputValue] = useState();
 
-    useEffect(() => {
-        checkConnection();
-    },[])
+  const wallet = useWallet();
 
-    const wallet = useWallet();
+  useEffect(() => {
+    checkConnection();
+  }, [wallet.status]);
 
-    const checkConnection = async () => {
-        let { ethereum } = window;
-        if (ethereum !== undefined) {
-            const chainId = await ethereum.request({ method: "eth_chainId" });
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const accounts = await provider.listAccounts();
 
-            if (accounts.length !== 0) {
-                onConnect();
-            }else if (accounts.length === 0)
-            {
-                console.log('Please connect wallet');
-            }
-        }
+  const checkConnection = async () => {
+    let { ethereum } = window;  
+    if (ethereum !== undefined) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const accounts = await provider.listAccounts();
+
+      if (accounts.length !== 0) {
+        onConnect();
+      } else if (accounts.length === 0) {
+        console.log("Please connect wallet");
+      }
     }
+  };
 
+  const onConnect = async () => {
+    if (wallet.status !== "connected") {
+      wallet.connect().catch((err) => {
+        console.error("please check metamask!");
+      });
+    }
+  };
 
-    const onConnect = async () => { 
-        if (wallet.status !== "connected") {
-            wallet.connect().catch((err) => {
-                console.error("please check metamask!");
-            });
-        }
-    };
+  useEffect(() => {
+    if (wallet.status == "connected") {
+      createQrCode();
+    }
+  }, [wallet.status]);
 
-    useEffect(() => {
-        console.log(wallet.status);
-        console.log(wallet.account);
-        const secret = speakeasy.generateSecret({name: wallet.account});
-        console.log(secret);
-        if(wallet.status == 'connected'){
-            // const secret = {
-            //     ascii: '?:SD%oDD<E!^q^1N):??&QkeqRkhkpt&',
-            //     base32: 'H45FGRBFN5CEIPCFEFPHCXRRJYUTUPZ7EZIWWZLRKJVWQ23QOQTA',
-            //     hex: '3f3a5344256f44443c45215e715e314e293a3f3f26516b6571526b686b707426',
-            //     otpauth_url:
-            //     'otpauth://totp/Adidas%Adidas?secret=H45FGRBFN5CEIPCFEFPHCXRRJYUTUPZ7EZIWWZLRKJVWQ23QOQTA',
-            // };
-            // console.log('SECRET -->', secret);
-            // Backup codes
-            const backupCodes = [];
-            const hashedBackupCodes = [];
-            // const randomCode = (Math.random() * 10000000000).toFixed();
-            // console.log('randomCode -->', randomCode);
-    
-            for (let i = 0; i < 10; i++) {
-                const randomCode = (Math.random() * 10000000000).toFixed();
-                const encrypted = CryptoJS.AES.encrypt(
-                  randomCode,
-                  secret.base32
-                ).toString();
-                backupCodes.push(randomCode);
-                hashedBackupCodes.push(encrypted);
-              }
-              console.log("backupCodes ----->", backupCodes);
-              //console.log("hashedBackupCodes ----->", hashedBackupCodes);
-              // const encrypted = CryptoJS.AES.encrypt(randomCode, secret.base32).toString();
-              // console.log('encrypted -->', encrypted)
-              // var bytes  = CryptoJS.AES.decrypt(encrypted, secret.base32);
-              // var originalText = bytes.toString(CryptoJS.enc.Utf8);
-              // console.log('originalText --->', originalText);
-          
-            QRCode.toDataURL(secret.otpauth_url, (err, image_data) => {
-                setImage(image_data);
-                setSecret(secret);
-            });
-        }
-    },[wallet.status])
+  const createQrCode = async () => {
+    const secret = speakeasy.generateSecret({ name: "Arcadian", length: 16 });
+    console.log(secret);
+    let createCheck;
+    await axios
+      .post("/api/users/", {
+        userAddress: wallet.account,
+        userhex: secret.hex,
+      })
+      .then((res) => {
+        createCheck = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    if (createCheck === 1) {
+      QRCode.toDataURL(secret.otpauth_url, (err, image_data) => {
+        setImage(image_data);
+        setSecret(secret);
+      });
+    }
+  }
 
-    const getCode = () => {
-        console.log("getCode");
-        const { base32, hex } = secret;
-        const code = speakeasy.totp({
-          secret: hex,
-          encoding: 'hex',
-          algorithm: 'sha1',
-        });
-        setValidCode(code);
-    };
+  const verifyCode = () => {
+    axios
+      .post("/api/users/verifyCode", {
+        userAddress: wallet.account,
+        checkCode: inputValue,
+      })
+      .then((res) => {
+        setIsCodeValid(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsCodeValid(false);
+      });
+  };
 
-    const verifyCode = () => {
-        const { base32, hex } = secret;
-        const isVerified = speakeasy.totp.verify({
-            secret: hex,
-            encoding: 'hex',
-            token: inputValue,
-            window: 1,
-        });
-
-        console.log('isVerified -->', isVerified);
-        setIsCodeValid(isVerified)
-    };
-
-
-    return (
+  return (
+    <div>
+      {wallet.status === "connected" ? (
+        <p>{wallet.account}</p>
+      ) : (
+        <button onClick={onConnect}>ConnectWallet</button>
+      )}
+      {wallet.status === "connected" && image != null ? (
         <div>
-            {wallet.status == "connected" ?<p>{wallet.account}</p> :<button onClick={onConnect}>ConnectWallet</button>}
-            {wallet.status == "connected" ? <img src={`${image}`} />: ""}
-            
-            {
-            <div>
-                <h2>{validCode}</h2>
-                <button onClick={getCode}>Get valid code</button>
-            </div>
-            }
-            <div style={{ marginTop: 20 }}>Verify code</div>
-            <input
-            type="number"
-            onChange={(e) => {setInputValue(e.target.value)}}
-            />
-            <button onClick={verifyCode}>Verify</button>
-            {isCodeValid !== null && <div>{isCodeValid ? 'Yes' : 'No'}</div>}
+        <img src={`${image}`} />
+        <p>secretkey: {secret.base32}</p>
+        <p>Plz save secretKey on secure place</p>
+        <button>got it</button>
         </div>
-    )
-}
+      ) : (
+        ""
+      )}
 
-export default GoogleAuthenticator
+      {/* {
+        <div>
+          <h2>{validCode}</h2>
+          <button onClick={getCode}>Get valid code</button>
+        </div>
+      } */}
+      <div style={{ marginTop: 20 }}>Verify code</div>
+      <input
+        type="number"
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <button onClick={verifyCode}>Verify</button>
+      {isCodeValid !== 0 && <div>{isCodeValid == true ? "Yes" : "No"}</div>}
+    </div>
+  );
+};
+
+export default GoogleAuthenticator;
